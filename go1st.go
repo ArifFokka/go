76d8783181967e297068514d63a5f8247beae8de
ArifFokka <mdarifall089@gmail.com>
2026-03-29 20:57:56 +0600

    TestGO-1st

---
+++ go1st.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+//tis is very fast
+/*arif cool edc */
+
+var (
+	A int = 1
+	B     = 3.14
+	C     = "Hi!"
+)
+
+func main() {
+
+	fmt.Println(A)
+	fmt.Println(B)
+	fmt.Println(C)
+	fmt.Print("hllow Arif")
+}
